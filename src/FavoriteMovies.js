8b8734c547b22898a20339e27672aaf0fd77ea43
Osamah Abdulaziz <mr.osamah.a@gmail.com>
2020-04-28 21:26:28 +0000

    First init from Udacity's Workspace!!

---
+++ src/FavoriteMovies.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class FavoriteMovies extends Component {
+  render(){
+    return (
+      <ol>
+      	{this.props.profiles.map((profile) => (
+      		<li>
+      			`${this.props.users[profile.userID].name}'s favorite movie is ${this.props.movies[profile.favoriteMovieID].name}`
+      		</li>
+    	))}
+      </ol>
+    )
+  }
+}
